fbc7a1eb553f3a15b75b6d9c6072d1affefaf909
Rajnil Saha <rajnilhere@gmail.com>
2025-11-25 22:55:38 +0530

    editing

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask , render_template , url_for , redirect
 from flask_bootstrap import Bootstrap
+
 app=Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -14,6 +16,10 @@ def index():
 @app.route("/about")
 def about():
   return render_template("about.html")
+
+@app.route("/css")
+def css():
+  return render_template("css.html")
 
 if __name__=="__main__":
   app.run(debug=True , port=5001)
