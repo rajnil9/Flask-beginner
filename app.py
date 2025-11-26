4ec3130e177ccf4f9930679258dee5d716687ce0
Rajnil Saha <rajnilhere@gmail.com>
2025-11-26 21:51:19 +0530

    update

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask , render_template , url_for , redirect
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
+import yaml
 
 app=Flask(__name__)
-Bootstrap(app)
+#Bootstrap(app)
+
+db=yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST']=db['mysql_host']
+app.config['MYSQL_USER']=db['mysql_user']
+app.config['MYSQL_PASSWORD']=db['mysql_password']
+app.config['MYSQL_DB']=db['mysql_db']
+mysql=MySQL(app)
 
 @app.route('/')
 def index():
+  cur=mysql.connection.cursor()
+  cur.execute("INSERT INTO user VALUES(%s)",['Mike'])
+  result_value=cur.execute("SELECT * FROM user")
+  if result_value>0:
+    users=cur.fetchall()
+    return users[0]
+    print(users)
   return render_template("index.html")
+  #return render_template("index.html")
   #fruits=["Apple","Mango","Orange"]
   #return render_template("index.html",fruits=fruits)
   #return render_template("index.html")
