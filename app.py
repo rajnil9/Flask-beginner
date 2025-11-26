f33baecea2cfe2e97cd82e1993e324ce9ec04ea3
Rajnil Saha <rajnilhere@gmail.com>
2025-11-26 22:12:16 +0530

    UPDATE

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask_mysqldb import MySQL
 import yaml
 
 app=Flask(__name__)
-#Bootstrap(app)
+Bootstrap(app)
 
-db=yaml.load(open('db.yaml'))
+db=yaml.safe_load(open('db.yaml'))
 app.config['MYSQL_HOST']=db['mysql_host']
 app.config['MYSQL_USER']=db['mysql_user']
 app.config['MYSQL_PASSWORD']=db['mysql_password']
@@ -16,11 +16,12 @@ mysql=MySQL(app)
 @app.route('/')
 def index():
   cur=mysql.connection.cursor()
-  cur.execute("INSERT INTO user VALUES(%s)",['Mike'])
+  #cur.execute("INSERT INTO user VALUES(%s)",['Mike'])
+  #mysql.connection.commit()
   result_value=cur.execute("SELECT * FROM user")
   if result_value>0:
     users=cur.fetchall()
-    return users[0]
+    return str(users[0])
     print(users)
   return render_template("index.html")
   #return render_template("index.html")
