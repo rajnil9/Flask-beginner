5b068b948db5ff412408f2daadff4dd5e38ac43b
Rajnil Saha <rajnilhere@gmail.com>
2025-11-29 21:10:46 +0530

    updating

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
-from flask import Flask , render_template , url_for , redirect
+from flask import Flask , render_template , url_for , redirect , request
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
 import yaml
 
 app=Flask(__name__)
-Bootstrap(app)
+#Bootstrap(app)
 
-db=yaml.safe_load(open('db.yaml'))
-app.config['MYSQL_HOST']=db['mysql_host']
-app.config['MYSQL_USER']=db['mysql_user']
-app.config['MYSQL_PASSWORD']=db['mysql_password']
-app.config['MYSQL_DB']=db['mysql_db']
-mysql=MySQL(app)
+#db=yaml.safe_load(open('db.yaml'))
+#app.config['MYSQL_HOST']=db['mysql_host']
+#app.config['MYSQL_USER']=db['mysql_user']
+#app.config['MYSQL_PASSWORD']=db['mysql_password']
+#app.config['MYSQL_DB']=db['mysql_db']
+#mysql=MySQL(app)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-  cur=mysql.connection.cursor()
+  if request.method=='POST':
+    return request.form['password']
+  return render_template("index.html")
+
+  #cur=mysql.connection.cursor()
   #cur.execute("INSERT INTO user VALUES(%s)",['Mike'])
   #mysql.connection.commit()
-  result_value=cur.execute("SELECT * FROM user")
-  if result_value>0:
-    users=cur.fetchall()
-    return str(users[0])
-    print(users)
-  return render_template("index.html")
+  #result_value=cur.execute("SELECT * FROM user")
+  #if result_value>0:
+   #users=cur.fetchall()
+    #return str(users[0])
+    #print(users)
+  #return render_template("index.html")
   #return render_template("index.html")
   #fruits=["Apple","Mango","Orange"]
   #return render_template("index.html",fruits=fruits)
